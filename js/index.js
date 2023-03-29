1542dc73e15ddf5f547823c0d6e6eae54c073cbd
Karapet Hambaryan <karohambaryan@gmail.com>
2023-03-30 00:44:57 +0400

    updates

--- js/index.js
+++ js/index.js
@@ -1,60 +1,27 @@
-//! JavaScriptDOM and his prototype metodes
-function JavaScriptDOM(idName) {
-	this.element = document.getElementById(idName);
 
+// curry :: ((a, b, ...) -> c) -> a -> b -> ... -> c
+function curry(fn) {
+	const arity = fn.length;
+
+	return function $curry(...args) {
+		if (args.length < arity) {
+			return $curry.bind(null, ...args);
+		}
+
+		return fn.call(null, ...args);
+	};
 }
 
-JavaScriptDOM.prototype.addClass = function (className) {
-	this.element.classList.add(className);
-};
-
-JavaScriptDOM.prototype.removeClass = function (className) {
-	this.element.classList.remove(className);
-};
-
-JavaScriptDOM.prototype.toggleClass = function (className) {
-	this.element.classList.toggle(className);
-};
-
-JavaScriptDOM.prototype.html = function (htmlString) {
-	if (htmlString) {
-		this.element.innerHTML = htmlString;
-	} else {
-		return this.element.innerHTML;
+// compose :: ((y -> z), (x -> y),  ..., (a -> b)) -> a -> z
+const compose = (...fns) => {
+	return (...args) => {
+		return fns.reduceRight((res, fn) => [fn.call(null, ...res)], args)[0];
 	}
-};
+}
 
-JavaScriptDOM.prototype.text = function (textString) {
-	if (textString) {
-		this.element.textContent = textString;
-	} else {
-		return this.element.textContent;
-	}
-};
 
-JavaScriptDOM.prototype.append = function (childNode) {
-	this.element.appendChild(childNode);
-};
-
-JavaScriptDOM.prototype.prepend = function (childNode) {
-	this.element.insertBefore(childNode, this.element.firstChild);
-};
-
-JavaScriptDOM.prototype.remove = function () {
-	this.element.parentNode.removeChild(this.element);
-};
-
-JavaScriptDOM.prototype.on = function (eventName, callback) {
-	this.element.addEventListener(eventName, callback);
-};
-
-JavaScriptDOM.prototype.off = function (eventName, callback) {
-	this.element.removeEventListener(eventName, callback);
-};
-
-JavaScriptDOM.prototype.trigger = function (eventName) {
-	var event = new Event(eventName);
-	this.element.dispatchEvent(event);
-};
-// ! ----------- end  JavaScriptDOM metodes block ----------
+const getRootElement = x => document.getElementById(x);
+const createDOMElement = x => document.createElement(x);
+const addClass = curry((x, ...cn) => x.classList.add(...cn));
+const addChildElement = curry((x, ch) => x.append(ch));
 
